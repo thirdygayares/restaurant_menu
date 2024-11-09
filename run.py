431f988ed209f81f6fd889fc46816c640a41acdf
Thirdy Gayares <gayares.j@labrador-company.com>
2024-11-09 19:50:05 +0800

    chore: add waitress

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+from waitress import serve
+
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main':
-    app.run(debug=True, use_reloader=True)
+    # app.run(debug=True, use_reloader=True)
+    serve(app, host='0.0.0.0', port=8080)
