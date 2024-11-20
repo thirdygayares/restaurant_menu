93100a62e305c604d14c48341fdf5ccce96b5343
Thirdy Gayares <gayaresthird@gmail.com>
2024-11-20 23:00:38 +0800

    fix: restaurant_category_repository.py

--- app/repository/restaurant_category_repository.py
+++ app/repository/restaurant_category_repository.py
@@ -1,36 +1,38 @@
+from datetime import datetime
+
 from app.extension import db
-from app.models.restaurant_model import RestaurantModel
+from app.models.restaurant_category_model import RestaurantCategoryModel
 
 
 class RestaurantCategoryRepository():
 
     @staticmethod
-    def create_restaurant(data):
-        restaurant = RestaurantModel(**data)
+    def create_restaurant_category(data):
+        restaurant = RestaurantCategoryModel(**data)
         db.session.add(restaurant)
         db.session.commit()
         return restaurant
 
     @staticmethod
-    def get_restaurant_by_id(restaurant_id):
-        return RestaurantModel.query.get(restaurant_id)
+    def get_restaurant_category_by_id(category_id):
+        return RestaurantCategoryModel.query.get(category_id)
 
     @staticmethod
-    def get_restaurant_by_uuid(restaurant_uuid):
-        return RestaurantModel.query.filter_by(restaurant_uuid=restaurant_uuid).first()
+    def get_restaurant_category_by_uuid(category_uuid):
+        return RestaurantCategoryModel.query.filter_by(category_uuid=category_uuid).first()
 
     @staticmethod
     def get_all():
-        return RestaurantModel.query.filter_by(deleted_at=None).all()
+        return RestaurantCategoryModel.query.filter_by(deleted_at=None).all()
 
     @staticmethod
-    def update_restaurant(restaurant, data):
+    def update_restaurant_category(restaurant, data):
         for key, value in data.items():
             setattr(restaurant, key, value)
         db.session.commit()
         return restaurant
 
     @staticmethod
-    def delete_restaurant(restaurant):
-        restaurant.deleted_at = RestaurantModel.utcnow()
+    def delete_restaurant_category(restaurant):
+        restaurant.deleted_at = datetime.utcnow()
         db.session.commit()
